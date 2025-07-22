f2421db8ca6b6ed3c54b9d1d0c14e1f3d9e13c30
Nhyl Bryle Ibañez <nbryleibanez@gmail.com>
2025-07-22 21:48:36 +0800

    init

---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+opt.tabstop = 4 -- Number of visual spaces per tab
+opt.shiftwidth = 4 -- Number of spaces per indent
+opt.expandtab = true -- Use spaces instead of tabs
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "ayu-theme/ayu-vim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ayu",
+    },
+  },
+}
